d6cc20947ad142d7b486999568b897269bdaade6
Bruno Montezano <brunodoyt@gmail.com>
2021-09-18 20:03:01 -0300

    alpha function renamed and better explained

--- R/alpha.R
+++ R/alpha.R
@@ -1,37 +1,38 @@
-#' Calculates Cronbach's alpha based on author's model
+#' Calculates Cronbach's alpha based on HCL-28 model
 #'
 #' @param data Dataset containing HCL-32 items.
 #'
 #' @return Cronbach's alpha coefficient calculated based on Pearson's correlation matrix in the full dataset, active factor and risk-taking factor.
 #' @export
 #'
-alpha_authors_model <- function(data) {
+alpha_hcl28 <- function(data) {
 
-  full <- data %>%
+  hcl28 <- data %>%
     dplyr::select(
-      y2, y3, y4, y5, y6, y10, y11, y12, y13,
-      y14, y15, y16, y17, y18, y19, y20, y21,
-      y22, y24, y28, y8, y9, y25, y26, y27,
+      y1, y2, y3, y4, y5, y6,
+      y7, y8, y9, y10, y11, y12,
+      y13, y14, y15, y18, y19, y20,
+      y22, y24, y25, y26, y27, y28,
       y29, y30, y31, y32
     )
 
   active <- data %>%
     dplyr::select(
       y2, y3, y4, y5, y6,
-      y10, y11, y12, y13,
-      y15, y16, y17, y18,
-      y19, y20, y21, y22,
+      y10, y11, y12, y13, y14,
+      y15, y18, y19, y20, y22,
       y24, y28
     )
 
   risk <- data %>%
     dplyr::select(
-      y8, y9, y25, y26, y27,
-      y29, y30, y31, y32
+      y1, y7, y8, y9, y25,
+      y26, y27, y29, y30,
+      y31, y32
     )
 
-  sets <- list(full, active, risk) %>%
-    purrr::set_names("Full data", "Active factor", "Risk-taking factor")
+  sets <- list(hcl28, active, risk) %>%
+    purrr::set_names("HCL-28", "Active factor", "Risk-taking factor")
 
   alphas <- purrr::map(sets,
     ~ psych::alpha(.x)) %>%
