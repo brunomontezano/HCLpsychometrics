c6c788d8c863c2f8c3afa644eaca5a175c18570c
Bruno Montezano <brunodoyt@gmail.com>
2021-09-18 18:19:23 -0300

    cronbachs alpha new function

--- R/alpha.R
+++ R/alpha.R
@@ -1,16 +1,43 @@
-#' Calculates ordinal alpha
+#' Calculates Cronbach's alpha based on author's model
 #'
 #' @param data Dataset containing HCL-32 items.
 #'
-#' @return Ordinal alpha coefficient calculated based on tetrachoric correlation matrix.
+#' @return Cronbach's alpha coefficient calculated based on Pearson's correlation matrix in the full dataset, active factor and risk-taking factor.
 #' @export
 #'
-ord_alpha <- function(data) {
+alpha_authors_model <- function(data) {
 
-  ordinal_alpha <- psych::alpha(
-    psych::tetrachoric(data)$rho
-  )
+  full <- data %>%
+    dplyr::select(
+      y2, y3, y4, y5, y6, y10, y11, y12, y13,
+      y14, y15, y16, y17, y18, y19, y20, y21,
+      y22, y24, y28, y8, y9, y25, y26, y27,
+      y29, y30, y31, y32
+    )
 
-  return(ordinal_alpha)
+  active <- data %>%
+    dplyr::select(
+      y2, y3, y4, y5, y6,
+      y10, y11, y12, y13,
+      y15, y16, y17, y18,
+      y19, y20, y21, y22,
+      y24, y28
+    )
+
+  risk <- data %>%
+    dplyr::select(
+      y8, y9, y25, y26, y27,
+      y29, y30, y31, y32
+    )
+
+  sets <- list(full, active, risk) %>%
+    purrr::set_names("Full data", "Active factor", "Risk-taking factor")
+
+  alphas <- purrr::map(sets,
+    ~ psych::alpha(.x)) %>%
+    purrr::map(purrr::pluck(1)) %>%
+    purrr::map(~ as.numeric(as.vector(.x)[2]))
+
+  return(alphas)
 
 }
